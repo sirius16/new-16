0283b17641cfd850fb32b566918e43055b5d6b8a
sirius16 <amozu16@gmail.com>
2015-07-16 11:49:34 -0400

    Update twitter16.js

--- twitter16.js
+++ twitter16.js
@@ -2,6 +2,6 @@ function hello() {
 	j = prompt("#", '0');
 	srcs = document.getElementsByClassName('js-display-url');
 	for (i = 0; i < j; i++) {
-		window.open(srcs[i].innerHTML);
+		window.open('http://'+srcs[i].innerHTML);
 	}
 }
